8b652722440d41d1da88e13d06d9743fe302cd86
Tim Holy <tim.holy@gmail.com>
2019-10-07 11:57:35 -0500

    Generate random stimulus sequence

--- src/Autosampler.jl
+++ src/Autosampler.jl
@@ -1,5 +1,46 @@
 module Autosampler
 
-greet() = print("Hello World!")
+using CSV, DataFrames
+using Random
+
+export stim_randomize, update_imagine
+
+"""
+    stim_randomize(filein, trials::Int, fileout=insertfilename(filein, "_sequence"))
+
+Generate a randomize sequence of stimuli. `filein` is a string containing the name
+of a CSV file that describes the unique stimuli (see format described in the README).
+`trials` specifies the number of pseudo-random sequences used to generate the
+final stimulus sequence.
+`fileout` is an optional argument used to control the name of the output file;
+the default is to insert "_sequence" right before the extension of `filein`.
+"""
+function stim_randomize(filein::AbstractString, trials::Int; fileout=insertfilename(filein, "_sequence"), kwargs...)
+    open(fileout, "w") do io
+        stim_randomize(io, filein, trials; kwargs...)
+    end
+    return fileout
+end
+
+function stim_randomize(io::IO, filein::AbstractString, trials::Int; writeheader=false, kwargs...)
+    df = CSV.File(filein) |> DataFrame!
+    println("Headers found: ", String.(names(df)))
+    n = size(df, 1)
+    println(n, " stimuli found")
+    p = Int[]
+    for i = 1:trials
+        append!(p, randperm(n))
+    end
+    CSV.write(io, df[p, :]; writeheader=writeheader, kwargs...)
+    flush(io)
+end
+
+function update_imagine()
+end
+
+function insertfilename(filename, tail)
+    basename, ext = splitext(filename)
+    return basename*tail*ext
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,27 @@
-using Autosampler
-using Test
+using Autosampler, ImagineFormat, CSV, DataFrames
+using Test, Random
 
 @testset "Autosampler.jl" begin
-    # Write your own tests here.
+    for stimfile in ("stimuli_tabs.csv", "stimuli_comma_quotes.csv")
+        Random.seed!(1234321)
+        mktemp() do pathcsv, iocsv
+            stim_randomize(iocsv, stimfile, 4)
+            str = read(pathcsv, String)
+            @test str == """
+            chemical1_10uM,RA1
+            chemical1_01uM,RA2
+            chemical1_10uM,RA1
+            chemical1_01uM,RA2
+            chemical1_01uM,RA2
+            chemical1_10uM,RA1
+            chemical1_10uM,RA1
+            chemical1_01uM,RA2
+            """
+            # mktemp() do pathi, ioi
+            #     update_imagine(ioi, "recording.imagine", pathcsv; pixelspacing=[0.3, 0.3, 5.2])
+            #     header = ImagineFormat.parse_header(pathi)
+            #     # @test header["stimulus"]
+            # end
+        end
+    end
 end
